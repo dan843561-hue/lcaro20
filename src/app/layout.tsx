00084d65f41ad1860c789d0d8cbdb630a887b6d0
dan843561-hue <dan843561-hue@gmail.com>
2026-09-08 19:13:49 -0500

    apk 9

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 // ✅ URL ABSOLUTA - Cambia solo si tu dominio es diferente
-const SITE_URL = "https://www.actualiza-red-ahora.online/";
+const SITE_URL = "hhttps://www.redmax-ahora-actualiza.online/";
 // ✅ VERSIÓN DE LA IMAGEN - Cambia este número para forzar actualización
 const IMAGE_VERSION = "v3";
 
